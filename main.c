f2157929540871c5012dffa57691dfffa7941fce
Seth Palmer <elderlord30@gmail.com>
2025-04-10 16:02:56 -0700

    First working test

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 int main(void) {
   // Setting D9 to high
   //
-  DDRB |= (1 << DDB1);
-  PORTB |= (1 << PORTB1);
+  DDRB |= (1 << PB1); // Set pin to output
+  // TCCR1 is a set of registers that control the clock and timer settings
+  TCCR1A |= (1 << COM1A1) |
+            (1 << WGM10); // PWM phase correct
+                          // Clear OC0A on compare match when up-count
+                          // Set OC0A on compare match when down-counting
+  TCCR1B |= (1 << WGM12) | (1 << CS11); // 1/8th prescaler
+                                        //
+  // ICR1 = 0;
+  OCR1A = 10; // set duty cycle (out of 255)
+
+  while (1)
+    ;
 }
